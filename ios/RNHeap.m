02aaa5b9e613f844dd762a004bff1f9677aedee6
Andy Isaacson <andy.isaacson@heapanalytics.com>
2018-12-03 15:38:44 -0600

    Making sure logPageview gets called first

--- ios/RNHeap.m
+++ ios/RNHeap.m
@@ -15,6 +15,8 @@
 
 RCT_EXPORT_MODULE();
 
+static BOOL pageViewSent = NO;
+
 - (dispatch_queue_t)methodQueue {
   return dispatch_get_main_queue();
 }
@@ -28,6 +30,21 @@ RCT_EXPORT_METHOD(enableVisualizer) {
 }
 
 RCT_EXPORT_METHOD(track:(NSString *)event withProperties:(NSDictionary *)properties) {
+    // The Heap library requires that a "page view" event be sent first, since properties
+    // will get copied down to manual events.  Unfortunately, the first page view happens
+    // before any JS code is run, and so the app doesn't yet have an ID.  This unfortunate
+    // snippet makes sure that a page view gets sent first.
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wundeclared-selector"
+    #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+    if (!pageViewSent) {
+        SEL logPageviewSelector = @selector(logPageview:);
+        if ([[Heap class] respondsToSelector:logPageviewSelector]) {
+            [[Heap class] performSelector:logPageviewSelector withObject:@{@"type": @"react-native"}];
+        }
+        pageViewSent = YES;
+    }
+    #pragma clang diagnostic pop
   [Heap track:event withProperties:properties];
 }
 
