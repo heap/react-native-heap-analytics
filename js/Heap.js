4e27a1e98b7723e28e141796cc5227e4e81a928d
Andy Isaacson <andy.isaacson@heapanalytics.com>
2018-12-11 14:26:14 -0600

    Enabling Android and adding first pass at Redux autotrack

--- js/Heap.js
+++ js/Heap.js
@@ -1,7 +1,8 @@
 // Libraries
-import {NativeModules, Platform} from 'react-native';
-import Package from 'react-native-package';
+import { NativeModules, Platform } from "react-native";
+import Package from "react-native-package";
 
+let flatten = require("flat");
 
 /**
  * Package.create handles two things:
@@ -15,29 +16,32 @@ import Package from 'react-native-package';
  * https://github.com/negativetwelve/react-native-package
  */
 export default Package.create({
-  json: require('../package.json'),
+  json: require("../package.json"),
   nativeModule: NativeModules.RNHeap,
   enabled: Platform.select({
     ios: true,
+    android: true
   }),
-  export: (Heap) => ({
+  export: Heap => ({
     // App Properties
-    setAppId: (appId) => Heap.setAppId(appId),
+    setAppId: appId => Heap.setAppId(appId),
 
     // User Properties
-    identify: (identity) => Heap.identify(identity),
-    addUserProperties: (properties) => Heap.addUserProperties(properties),
+    identify: identity => Heap.identify(identity),
+    addUserProperties: properties => Heap.addUserProperties(properties),
 
     // Event Properties
-    addEventProperties: (properties) => Heap.addEventProperties(properties),
-    removeEventProperty: (property) => Heap.removeEventProperty(property),
+    addEventProperties: properties => Heap.addEventProperties(properties),
+    removeEventProperty: property => Heap.removeEventProperty(property),
     clearEventProperties: () => Heap.clearEventProperties(),
 
     // Events
     track: (event, payload) => Heap.track(event, payload),
 
-    // Config
-    enableVisualizer: () => Heap.enableVisualizer(),
-    changeInterval: (interval) => Heap.changeInterval(interval),
-  }),
+    // Redux middleware
+    reduxMiddleware: store => next => action => {
+      Heap.track("Redux Action", flatten(action));
+      next(action);
+    }
+  })
 });
